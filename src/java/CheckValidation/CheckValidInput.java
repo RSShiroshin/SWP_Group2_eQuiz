7f2615e4e2b2dfbc2998cc6c5ea14e1767bb4ee4
ThanhQuangT <Thanhnqt2001@gmail.com>
2022-11-03 08:55:49 +0700

    CheckVaild

---
+++ src/java/CheckValidation/CheckValidInput.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CheckValidation;
+
+/**
+ *
+ * @author hello
+ */
+public class CheckValidInput {
+    private static String name_Vaild = "[A-Za-z]{5,15}";
+    private static String fullname_Vaild = "[A-Za-z ]{5,25}";
+    private static String email_Vaild = "[a-zA-Z0-9]{5,20}+[@]{1}+[a-z]{3,9}+[.]{1}+[a-z]{2,9}";
+
+}
