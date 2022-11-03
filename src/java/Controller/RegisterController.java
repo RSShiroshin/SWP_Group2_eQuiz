6ac31f4f7aa1b091eb32366af6394c47524bca0a
ThanhQuangT <Thanhnqt2001@gmail.com>
2022-11-03 08:44:57 +0700

    NewDao

--- src/java/Controller/RegisterController.java
+++ src/java/Controller/RegisterController.java
@@ -16,6 +16,7 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -32,9 +33,9 @@ public class RegisterController extends HttpServlet {
     }
 
     public static void SendEmail(String getEmail) throws MessagingException, UnsupportedEncodingException {
-        
+
         //Email cua ban
-        final String fromEmail = ""; 
+        final String fromEmail = "";
         // Mat khai email cua ban
         final String password = "";
         // dia chi email nguoi nhan
@@ -132,10 +133,8 @@ public class RegisterController extends HttpServlet {
         String date_raw = "2022-10-18";
         String fullname = request.getParameter("fullname");
         Date date = Date.valueOf(date_raw);
-        User acc = userDAO.checkDupAcc(username,fullname,email);
-        if (acc != null) {
-            error = "Account is existed!";
-            request.setAttribute("error", error);
+        if (checkDupInfor(userDAO,username, email, fullname) == false) {
+            checkDupInput(request, userDAO, username, email, fullname);
             userDAO.closeConnection();
             request.getRequestDispatcher("View/register.jsp").forward(request, response);
         } else {
@@ -169,6 +168,37 @@ public class RegisterController extends HttpServlet {
     public static String convertByteToString(byte[] byteValue) {
         String stringValue = "" + Arrays.toString(byteValue);
         return (stringValue);
+    }
+
+    public boolean checkDupInfor(UserDAO userDAO,String username, String email, String fullname) {
+        User checkDupUsername = userDAO.checkDupAcc(username);
+        User checkDupEmail = userDAO.checkDupAcc(email);
+        User checkDupFullName = userDAO.checkDupAcc(fullname);
+        if (checkDupFullName != null || checkDupEmail != null || checkDupUsername != null) {
+            return false;
+        }
+        return true;
+    }
+
+    public void checkDupInput(HttpServletRequest request, UserDAO userDAO, String username, String email, String fullname) {
+        String usererror = "";
+        String emailerror = "";
+        String fullnameerror = "";
+        User checkDupUsername = userDAO.checkDupAcc(username);
+        User checkDupEmail = userDAO.checkDupAcc(email);
+        User checkDupFullName = userDAO.checkDupAcc(fullname);
+        if (checkDupUsername != null) {
+            usererror = "Duplicate Username";
+            request.setAttribute("usererror", usererror);
+        }
+        if (checkDupEmail != null) {
+            emailerror = "Duplicate email";
+            request.setAttribute("emailerror", emailerror);
+        }
+        if (checkDupFullName != null) {
+            fullnameerror = "Duplicate fullname";
+            request.setAttribute("fullnameerror", fullnameerror);
+        }
     }
 
     /**
